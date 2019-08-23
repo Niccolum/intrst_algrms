3ddcfe56faa16b633d92b5f1214f858f75fad0ed
Niccolum <nikolai_vidov@epam.com>
2019-08-23 17:29:06 +0300

    add BUILD_NUMBER

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name="Intrst_algrms",
-    version=read('BUILD_NUMBER'),
+    version=read('BUILD_NUMBER.md'),
     author="Nikolai Vidov",
     author_email="lastsal@mail.ru",
     description="https://intrst-algrms.readthedocs.io/en/latest/",
