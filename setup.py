b447d05352592939e0d764cb75e816f8411b5964
Niccolums <lastsal93@gmail.com>
2019-08-24 21:58:07 +0300

    fix line-profiler

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 import os
 import sys
+
 from setuptools import setup
 
 if sys.version_info < (3, 6):
     raise RuntimeError("Intrst_algrms 3.x requires Python 3.6+")
+
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
@@ -17,7 +19,7 @@ setup(
     description="https://intrst-algrms.readthedocs.io/en/latest/",
     license="MIT",
     keywords="example documentation algorithms",
-    url="https://intrst-algrms.readthedocs.io/en/latest/",
+    url="https://pypi.org/project/Intrst-algrms/",
     packages=['.', 'binary_tree', 'knapsack_problem', 'unpacking_flatten_lists'],
     package_data={
         # If any package contains files, include them:
@@ -29,7 +31,7 @@ setup(
         'iteration-utilities',
         'more-itertools',
         'memory-profiler',
-        'line-profiler',
+        # 'line-profiler', # install line-profiler manually
         'matplotlib',
     ],
     long_description=read('README.md'),
